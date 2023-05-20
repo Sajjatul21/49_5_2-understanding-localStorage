645276cff5fce50e4a33e98abdf915f556a9a806
Sajjatul21 <sajjatulislam565@gmail.com>
2023-05-20 05:31:10 +0100

    understanding local sotrage

---
+++ script.js
@@ -0,0 +1,8 @@
+const addProduct = () => {
+    const product = document.getElementById("product").value;
+    const quentity = document.getElementById("quentity").value;
+    console.log(product, quentity);
+   
+
+};
+// const cart = { pen: 4, tripod: 2, handSanitizer: 2 }
