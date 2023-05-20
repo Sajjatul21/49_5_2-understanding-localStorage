7e258a2e56e47c42c26984247c28958bc3c4f498
Sajjatul21 <sajjatulislam565@gmail.com>
2023-05-20 05:36:02 +0100

    we create a empty object named cart then we set cart object key is productg and value is quentity and set cart on local sotrage

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const addProduct = () => {
     const product = document.getElementById("product").value;
     const quentity = document.getElementById("quentity").value;
-    console.log(product, quentity);
-   
+    // console.log(product, quentity);
+    const cart = {};
+    cart[product] = quentity;
+    localStorage.setItem("cart", JSON.stringify(cart))
+
 
 };
 // const cart = { pen: 4, tripod: 2, handSanitizer: 2 }
