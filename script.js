4686eb55a21cc69bb2d68529b9b1d98075d0f490
Sajjatul21 <sajjatulislam565@gmail.com>
2023-05-20 05:46:24 +0100

    we set getStoredCarty in cart. if  local storage haven	 value then we store empty object on cart and if local have value then we parse it and set the value in cart

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const addProduct = () => {
     const product = document.getElementById("product").value;
     const quentity = document.getElementById("quentity").value;
     // console.log(product, quentity);
-    const cart = {};
+    const cart = getStoredCart();
     cart[product] = quentity;
     localStorage.setItem("cart", JSON.stringify(cart))
 
@@ -12,7 +12,7 @@ const getStoredCart = () => {
     const storedCart = localStorage.getItem("cart");
     let cart = {};
     if (storedCart) {
-        cart = JSON.parse(storedCart);  
+        cart = JSON.parse(storedCart);
     }
     else {
         return cart;
